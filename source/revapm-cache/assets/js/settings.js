10b0ae33f30e8a11362b3f4c32d9ddb672a06d9e
iliaivanov2016 <infinity-ex@narod.ru>
2016-08-01 23:39:32 +0400

    v.0.0.3 account registration, api keys, domains creation / deletion replacement of static files (css,js, images) to cdn

--- source/revapm-cache/assets/js/settings.js
+++ source/revapm-cache/assets/js/settings.js
@@ -1,6 +1,5 @@
-function mcrevapm_save_settings(){
-	console.log("save settings");
-}
+var mcrevapm_domain = window.location.hostname;
+var mcrevapm_counter = 0;
 
 function mcrevapm_create_account(el){
 	jQuery(el).prop("disabled",true);
@@ -20,9 +19,12 @@ function mcrevapm_create_account(el){
 					jQuery(el).prop("disabled",false);
 					jQuery(el).find(".mcrevapm_spinner").addClass("mcrevapm_hidden");
 					if (parseInt(res.statusCode) != 200) {
+						// error
 						jQuery("#mcrevapm_tab2 .mcrevapm_err").html(res.message).show();
 					}
 					else {
+						// ok
+						jQuery("#mcrevapm_signup").find('input[type="text"],input[type="email"],input[type="password"],select').prop("disabled",true);
 						jQuery("#mcrevapm_tab2 .mcrevapm_ok").html(res.message).show();
 						jQuery(el).addClass("mcrevapm_hidden");
 						jQuery(el).next().removeClass("mcrevapm_hidden");
@@ -32,23 +34,256 @@ function mcrevapm_create_account(el){
 	});
 }
 
-function mcrevapm_get_api_key(){
-	alert("Not implemented yet!");
+function mcrevapm_get_api_key(el){
+	jQuery(el).prop("disabled",true);
+	jQuery(el).find(".mcrevapm_spinner").removeClass("mcrevapm_hidden");
+	jQuery("#mcrevapm_tab2 .mcrevapm_err").hide();
+	jQuery("#mcrevapm_tab2 .mcrevapm_ok").hide();
+	jQuery.post( MCREVAPM_AJAX_URL,{
+		"action": "mcrevapmgetapikey",
+		"email": ""+jQuery("#mcrevapm_email").val(),
+		"pass": ""+jQuery("#mcrevapm_pass").val()
+	}, function(res) {
+//console.log(res);
+		jQuery(el).prop("disabled",false);
+		jQuery(el).find(".mcrevapm_spinner").addClass("mcrevapm_hidden");
+//console.log(res); console.log("statusCode = "+parseInt(res.statusCode) );
+		if (parseInt(res.statusCode) != 200) {
+			// error
+			jQuery("#mcrevapm_tab2 .mcrevapm_err").html(res.message).show();
+		} else {
+			jQuery("#mcrevapm_tab2 .mcrevapm_ok").html(res.message).show();
+			jQuery(el).hide();
+			window.location.reload();
+		}
+	});
+}
+
+function mcrevapm_update_api_key(){
+	var new_key = jQuery("#mcrevapm_api_key").val().toString();
+	var old_key;
+	try{
+		old_key = MCREVAPM_SETTINGS.api_key.toString();
+	} catch(e) { if (console) console.log("Error getting old api key\n"+e.message); return; }
+//console.log("new_key = "+new_key);console.log("old_key = "+old_key);
+	if (old_key == new_key) return;
+	if (jQuery("#mcrevapm_api_key_updating_div").hasClass("mcrevapm_hidden")) {
+//console.log("has class - starting...");
+		jQuery("#mcrevapm_api_key_updating_div").removeClass("mcrevapm_hidden");
+		if (confirm("Update API Key. Are you sure?")) {
+//console.log("has class - starting...confirmed!");
+			jQuery.post(MCREVAPM_AJAX_URL,{
+				"action": "mcrevapmsetapikey",
+				"key":    new_key
+			}, function (res){
+//console.log(res);
+				try{
+					jQuery("#mcrevapm_api_key_updating_div").addClass("mcrevapm_hidden");
+					if (parseInt(res.statusCode) != 200) {
+						alert(res.message);
+						jQuery("#mcrevapm_api_key").val(old_key);
+						return;
+					}
+					jQuery("#mcrevapm_cdn_status").prop("checked",false);
+					// update domains list
+					jQuery("#mcrevapm_domains_table tbody").html("");
+					for (var key in res.data) {
+						var d = res.data[key];
+						jQuery("#mcrevapm_domains_table tbody").append('<tr id="mcrevapm_domain_row_'+d.id+'" data-id="'+d.id+'">'
+								+'<td><input type="radio" name="mcrevapm_rg_domains" class="mcrevapm_rg_domains" /></td>'
+								+'<td>'+d.domain_name+'</td>'
+								+'<td>'+d.origin_server+'</td>'
+								+'<td>'+d.origin_host_header+'</td>'
+								+'<td>&nbsp;</td>'
+								+'<td>'+d.cname+'</td>'
+								+'<td>'
+//									+'<i class="fa fa-pencil mcrevapm_edit_domain" aria-hidden="true" onclick="mcrevapm_edit_domain(this)" title="Edit Domain"></i>'
+									+'<i class="fa fa-times mcrevapm_delete_domain" aria-hidden="true" onclick="mcrevapm_delete_domain(this)" title="Delete Domain"></i>'
+								+'</td>'
+								+'</tr>'
+							);
+					}
+				} catch(e) { if (console) console.log("Update API key error:\n"+e.message); jQuery("#mcrevapm_api_key").val(old_key); }
+			});
+		} else {
+			jQuery("#mcrevapm_api_key").val(old_key);
+			jQuery("#mcrevapm_api_key_updating_div").addClass("mcrevapm_hidden");
+			return;
+		}
+	}
+}
+
+function mcrevapm_add_domain(){
+	mcrevapm_counter++;
+	var id='mcrevapm_domain_row_'+mcrevapm_counter;
+	jQuery("#mcrevapm_domains_table tbody").append('<tr id="'+id+'" data-id="" >'
+		+'<td><input type="radio" name="mcrevapm_rg_domains" class="mcrevapm_rg_domains" disabled="disabled" /></td>'
+		+'<td><input type="text" class="mcrevapm_domain" value="'+mcrevapm_domain+'"/></td>'
+		+'<td><input type="text" class="mcrevapm_server" value="'+mcrevapm_domain+'"/></td>'
+		+'<td><input type="text" class="mcrevapm_header" value="'+mcrevapm_domain+'"/></td>'
+		+'<td><select class="mcrevapm_location">'+MCREVAPM_LOCATIONS+'</select></td>'
+		+'<td>&nbsp;</td>'
+		+'<td>'
+			+'<i class="fa fa-check mcrevapm_submit_domain" aria-hidden="true" onclick="mcrevapm_submit_add_domain(this)" title="Save New Domain"></i>'
+			+'<i class="fa fa-times mcrevapm_delete_domain" aria-hidden="true" onclick="mcrevapm_delete_domain(this)" title="Delete Domain"></i>'
+		+'</td>'
+		+'</tr>'
+	);
+}
+
+function mcrevapm_submit_add_domain(el){
+	var loc = ""+jQuery(el).parents("tr").find(".mcrevapm_location").val();
+	if ((loc.length <= 0) || (loc == "null")) {
+		alert("You should select a location");
+		return;
+	}
+	var request = {
+		"action": "mcrevapmadddomain",
+		"domain": ""+jQuery(el).parents("tr").find(".mcrevapm_domain").val(),
+		"server": ""+jQuery(el).parents("tr").find(".mcrevapm_server").val(),
+		"header": ""+jQuery(el).parents("tr").find(".mcrevapm_header").val(),
+		"location": ""+jQuery(el).parents("tr").find(".mcrevapm_location").val()
+	};
+//console.log("add domain"); console.log(request);
+	jQuery.post(MCREVAPM_AJAX_URL,request, function(res){
+//console.log(res);
+		if (parseInt(res.statusCode) != 200) {
+			alert(res.message);
+		} else {
+			jQuery(el).parents("tr").find("td:last").html(jQuery("#mcrevapm_domain_progress_container").html());
+		}
+	});
+	//alert("submit add");
+}
+
+function mcrevapm_delete_domain(el){
+	if (!confirm("Delete domain. Are you sure?")) return;
+	var id = "" + jQuery(el).parents("tr").attr("data-id");
+	if (id.length < 5) {
+		jQuery(el).parents("tr").remove();
+		return;
+	}
+	var request = {
+		"action": "mcrevapmadeletedomain",
+		"id": id
+	};
+//console.log("delete domain id = "+id);
+	jQuery.post(MCREVAPM_AJAX_URL,request, function(res){
+//console.log(res);
+		if (parseInt(res.statusCode) != 200) {
+			alert(res.message);
+		} else {
+			jQuery(el).parents("tr").remove();
+		}
+	});
+}
+
+/*
+function mcrevapm_edit_domain(el){
+	var id = "" + jQuery(el).parents("tr").attr("data-id");
+	alert("edit" + id);
+}
+*/
+
+function mcrevapm_save_system_settings(){
+	//console.log("save system settings");
+	if (!jQuery("#mcrevapm_cdn_enabled_progress_div").hasClass("mcrevapm_hidden")) return;
+	jQuery("#mcrevapm_cdn_enabled_progress_div").removeClass("mcrevapm_hidden");
+	var request = {
+		"cdn_status": (jQuery("#mcrevapm_cdn_status").is(":checked")) ? "on" : "off"
+	};
+	if (request.cdn_status == "on") {
+		var row_el = jQuery(".mcrevapm_rg_domains:checked").eq(0);
+		request["domain_id"] = row_el.parents("tr").attr("data-id");
+		request["domain_name"] = row_el.parents("tr").find("td").eq(1).html();
+		request["domain_server"] = row_el.parents("tr").find("td").eq(2).html();
+		request["domain_cname"] = row_el.parents("tr").find("td").eq(5).html();
+	}
+//console.log(request);
+	jQuery.ajax({
+		  type:             'POST',
+      url:              MCREVAPM_AJAX_URL+"?action=mcrevapmsavesystemsettings",
+		  contentType:      'application/json',
+      data:             JSON.stringify(request),
+      dataType:         "json",
+      success: function(res){
+//console.log(res);
+	      try{
+					jQuery("#mcrevapm_cdn_enabled_progress_div").addClass("mcrevapm_hidden");
+		      alert("System savings saved!");
+				} catch(e) { if (console) console.log(e.message); }
+      }
+	});
+
 }
 
 function mcrevapm_purge_all(){
-
+	alert ("Not implemented yet")
 }
 
+
 jQuery(document).ready(function(){
-console.log("ready settings page"); console.log(MCREVAPM_SETTINGS);
+//console.log("ready settings page"); console.log(MCREVAPM_SETTINGS);
+		var key = "";
+		if (MCREVAPM_SETTINGS.hasOwnProperty("api_key"))
+			key += MCREVAPM_SETTINGS.api_key;
+//console.log(key);
+		if (key.length > 10) {
 	  jQuery("#mcrevapm_tabs").tabs({
-        "active": 1
+        "active": 0
     });
+		} else {
+		  jQuery("#mcrevapm_tabs").tabs({
+	        "active": 1
+	    });
+		}
 		jQuery("#mcrevapm_settings_div").show();
+		jQuery("#mcrevapm_plan").html("");
+		for (var key in MCREVAPM_BILLING_PLANS) {
+			var plan = MCREVAPM_BILLING_PLANS[key];
+			var name = "" + plan.name + " $" + plan.monthly_fee+"/mon";
+			jQuery("#mcrevapm_plan").append('<option value="'+key+'">'+name+'</option>');
+		}
 		jQuery("#mcrevapm_plan").on("change",function(){
-			jQuery(".mcrevapm_plan").addClass("mcrevapm_hidden");
-			var plan = ""+jQuery(this).val();
-			jQuery(".mcrevapm_plan_"+plan).removeClass("mcrevapm_hidden");
+			var key = "" + jQuery(this).val();
+			try{
+				var plan = MCREVAPM_BILLING_PLANS[key];
+				jQuery(".mcrevapm_plan_description").html(plan.description);
+				jQuery(".mcrevapm_plan_price").html(plan.monthly_fee);
+				jQuery(".mcrevapm_plan_price1").html(plan.services[0].cost);
+				jQuery(".mcrevapm_plan_unit1").html(plan.services[0].measurement_unit);
+				jQuery(".mcrevapm_plan_price2").html(plan.services[1].cost);
+				jQuery(".mcrevapm_plan_unit2").html(plan.services[1].measurement_unit);
+				jQuery(".mcrevapm_plan_price3").html(plan.services[2].cost);
+				jQuery(".mcrevapm_plan_unit3").html(plan.services[2].measurement_unit);
+				jQuery(".mcrevapm_plan_price4").html(plan.services[3].cost);
+				jQuery(".mcrevapm_plan_unit4").html(plan.services[3].measurement_unit);
+				jQuery(".mcrevapm_plan_price5").html(plan.services[4].cost);
+				jQuery(".mcrevapm_plan_unit5").html(plan.services[4].measurement_unit);
+			} catch(e) { if (console) console.log("Error in setting plan key = "+key+"\n"+e.message);}
+		}).trigger("change");
+		jQuery("#mcrevapm_api_key").bind("propertychange blur paste", function (){
+//console.log("api key changed");
+			mcrevapm_update_api_key();
+		}).on("keypress",function (e){
+			if ( e.which == 13 ) {
+        e.preventDefault();
+				mcrevapm_update_api_key();
+      }
 		});
+	jQuery("#mcrevapm_cdn_status").on("click",function(e){
+		if (jQuery("#mcrevapm_cdn_status").is(":checked")) {
+			var qty = jQuery(".mcrevapm_rg_domains:checked").length;
+//console.log("checked = "+qty);
+			if (qty <= 0) {
+				jQuery("#mcrevapm_cdn_status").prop("checked",false);
+				alert("You should select a domain to use RevAPM CDN");
+				return;
+			}
+		}
+		mcrevapm_save_system_settings();
+	});
+	jQuery(".mcrevapm_rg_domains").on("click",function(e){
+		jQuery("#mcrevapm_cdn_status").prop("checked",false).trigger("click");
+	});
 });
--- source/revapm-cache/revapm-cache.php
+++ source/revapm-cache/revapm-cache.php
@@ -6,17 +6,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 Plugin Name: RevAPM Cache
 Plugin URI: https://www.revapm.com/
 Description: A plugin for wordpress cache based on RevAPM CDN network
-Version: 0.0.1
+Version: 0.0.3
 Author: Modular Coding Inc
 Author URI: https://modcoding.com
 License: See license.txt
 */
-define ("MCREVAPM_PLUGIN_VERSION","ver=0.0.1");
+define ("MCREVAPM_PLUGIN_VERSION","ver=0.0.3");
 define ("MCREVAPM_PLUGIN_DIR",str_replace(DIRECTORY_SEPARATOR,"/",plugin_dir_path( __FILE__ )));
 define ("MCREVAPM_PLUGIN_URL",plugins_url()."/revapm-cache/");
 define ("MCREVAPM_SITE_URL",site_url()."/");
 define ("MCREVAPM_AJAX_URL",admin_url('admin-ajax.php'));
 define ("MCREVAPM_DEBUG",1);
+//define ("MCREVAPM_DEBUG_CURL",1);
+define ("MCREVAPM_TESTS_ENABLED",1);
 global $table_prefix;
 //define ("MCREVAPM_AREA_TABLE",$table_prefix."revapm_settings");
 if (defined("MCREVAPM_DEBUG")) {
@@ -49,7 +51,12 @@ if (defined("MCREVAPM_DEBUG")) mcrevapm_log(">activation settings:\n".$s);
 	if (strlen($s) <= 0) {
 		$ar_default_settings = array(
 			"cdn_status" => "off",
-			"account_status" => ""
+			"account_status" => "",
+			"api_key" => "",
+			"domain_id" => "",
+			"domain_name" => "",
+			"domain_server" => "",
+			"domain_cname" => ""
 		);
 		add_option("mcrevapm_settings",json_encode($ar_default_settings));
 	}
@@ -57,6 +64,7 @@ if (defined("MCREVAPM_DEBUG")) mcrevapm_log(">activation settings:\n".$s);
 
 function mcrevapm_deactivation(){
 	// do nothing for now
+	delete_option("mcrevapm_settings");
 }
 
 function mcrevapm_settings(){
@@ -80,13 +88,22 @@ function mcrevapm_header(){
 if (defined("MCREVAPM_DEBUG")) mcrevapm_log(">header");
 }
 
-function mcrevapm_shutdown(){
+function mcrevapm_footer(){
 	// TODO: filter content and replace static links to CDN if enabled
-if (defined("MCREVAPM_DEBUG")) mcrevapm_log(">shutdown");
-	$s = @ob_get_contents();
+if (defined("MCREVAPM_DEBUG")) mcrevapm_log(">footer");
+	$s = @ob_get_clean();
+	$ar_settings = @json_decode(get_option("mcrevapm_settings"),true);
+if (defined("MCREVAPM_DEBUG")) mcrevapm_log("1.footer\n".print_r($ar_settings,true));
+	if (@$ar_settings["cdn_status"] == "on") {
+		$server = $_SERVER["HTTP_HOST"];
+if (defined("MCREVAPM_DEBUG")) mcrevapm_log("2.footer server = ".$server);
+		$new_server = $ar_settings["domain_cname"];
+if (defined("MCREVAPM_DEBUG")) mcrevapm_log("3.footer new server = ".$new_server);
+		$s = str_replace($server,$new_server,$s);
+	}
 	echo $s;
 if (defined("MCREVAPM_DEBUG")) file_put_contents(MCREVAPM_PLUGIN_DIR."page.html",$s);
-	if (defined("MCREVAPM_DEBUG")) mcrevapm_log("<shutdown");
+	if (defined("MCREVAPM_DEBUG")) mcrevapm_log("<footer");
 }
 
 function mcrevapm_save_settings(){
@@ -109,8 +126,8 @@ $plugin = plugin_basename(__FILE__);
 add_filter("plugin_action_links_$plugin", 'mcrevapm_settings_link');
 //if (!is_admin() && ((int)$_GET["mcdebug"] == 1)) {
 if (!is_admin())  {
-	add_action('init', 'mcrevapm_header', 20);
-	add_action('shutdown', 'mcrevapm_shutdown', 20);
+	add_action('init', 'mcrevapm_header');
+	add_action('wp_footer', 'mcrevapm_footer', PHP_INT_MAX);
 //	remove_action( 'shutdown', 'wp_ob_end_flush_all', 1 );
 }
 
@@ -118,12 +135,86 @@ if (!is_admin())  {
 function mcrevapm_signup(){
 if (defined("MCREVAPM_DEBUG")) mcrevapm_log(">mcrevapm_signup");
 	include_once MCREVAPM_PLUGIN_DIR."inc/api.php";
-if (defined("MCREVAPM_DEBUG")) mcrevapm_log("1.mcrevapm_signup");
 	$api = new MCREVAPM_API();
-if (defined("MCREVAPM_DEBUG")) mcrevapm_log("2.mcrevapm_signup");
-	echo $api->signup();
+	$data = @json_decode(@file_get_contents("php://input"),true);
+	echo $api->signup($data);
 if (defined("MCREVAPM_DEBUG")) mcrevapm_log("<mcrevapm_signup");
 	exit(0);
 }
 add_action("wp_ajax_mcrevapmsignup","mcrevapm_signup");
-add_action("wp_ajax_nopriv_mcrevapmsignup","mcrevapm_signup");
+
+function mcrevapm_get_api_key(){
+if (defined("MCREVAPM_DEBUG")) mcrevapm_log(">mcrevapm_get_api_key");
+	include_once MCREVAPM_PLUGIN_DIR."inc/api.php";
+	$api = new MCREVAPM_API();
+	$email = trim(@$_REQUEST["email"]);
+	$password = @$_REQUEST["pass"];
+	echo $api->get_api_key($email,$password);
+if (defined("MCREVAPM_DEBUG")) mcrevapm_log("<mcrevapm_get_api_key");
+	exit(0);
+}
+add_action("wp_ajax_mcrevapmgetapikey","mcrevapm_get_api_key");
+
+function mcrevapm_set_api_key() {
+if (defined("MCREVAPM_DEBUG")) mcrevapm_log(">mcrevapm_set_api_key\n".print_r($_REQUEST,true));
+	include_once MCREVAPM_PLUGIN_DIR."inc/api.php";
+	$api = new MCREVAPM_API();
+	$key = @$_REQUEST["key"];
+	echo $api->set_api_key($key);
+if (defined("MCREVAPM_DEBUG")) mcrevapm_log("<mcrevapm_set_api_key");
+	exit(0);
+}
+add_action("wp_ajax_mcrevapmsetapikey","mcrevapm_set_api_key");
+
+function mcrevapm_add_domain() {
+if (defined("MCREVAPM_DEBUG")) mcrevapm_log(">mcrevapm_add_domain\n".print_r($_REQUEST,true));
+	include_once MCREVAPM_PLUGIN_DIR."inc/api.php";
+	$api = new MCREVAPM_API();
+	$domain = @$_REQUEST["domain"];
+	$server = @$_REQUEST["server"];
+	$header = @$_REQUEST["header"];
+	$location = @$_REQUEST["location"];
+	echo $api->add_domain($domain,$server,$header,$location);
+if (defined("MCREVAPM_DEBUG")) mcrevapm_log("<mcrevapm_add_domain");
+	exit(0);
+}
+add_action("wp_ajax_mcrevapmadddomain","mcrevapm_add_domain");
+
+function mcrevapm_delete_domain() {
+if (defined("MCREVAPM_DEBUG")) mcrevapm_log(">mcrevapm_delete_domain\n".print_r($_REQUEST,true));
+	include_once MCREVAPM_PLUGIN_DIR."inc/api.php";
+	$api = new MCREVAPM_API();
+	$id = @$_REQUEST["id"];
+	echo $api->delete_domain($id);
+if (defined("MCREVAPM_DEBUG")) mcrevapm_log("<mcrevapm_delete_domain");
+	exit(0);
+}
+add_action("wp_ajax_mcrevapmadeletedomain","mcrevapm_delete_domain");
+
+function mcrevapm_save_system_settings() {
+if (defined("MCREVAPM_DEBUG")) mcrevapm_log(">mcrevapm_save_system_settings\n".print_r($_REQUEST,true));
+	if (!current_user_can("manage_options")) die ("Access denied");
+	$json_data = @file_get_contents("php://input");
+	$request = @json_decode($json_data, true);
+if (defined("MCREVAPM_DEBUG")) mcrevapm_log("1.mcrevapm_save_system_settings request:\n".print_r($request,true));
+	$ar_settings = @json_decode(get_option("mcrevapm_settings"),true);
+if (defined("MCREVAPM_DEBUG")) mcrevapm_log("2.mcrevapm_save_system_settings old settings:\n".print_r($ar_settings,true));
+	$ar_settings["cdn_status"] = trim(@$request["cdn_status"]);
+	if ($ar_settings["cdn_status"] == "on") {
+			$ar_settings["domain_id"] = trim(@$request["domain_id"]);
+			$ar_settings["domain_name"] = trim(@$request["domain_name"]);
+			$ar_settings["domain_server"] = trim(@$request["domain_server"]);
+			$ar_settings["domain_cname"] = trim(@$request["domain_cname"]);
+	}
+	update_option("mcrevapm_settings",json_encode($ar_settings));
+if (defined("MCREVAPM_DEBUG")) mcrevapm_log("3.mcrevapm_save_system_settings new settings:\n".print_r($ar_settings,true));
+	echo "1";
+if (defined("MCREVAPM_DEBUG")) mcrevapm_log("<mcrevapm_save_system_settings");
+	exit(0);
+}
+add_action("wp_ajax_mcrevapmsavesystemsettings","mcrevapm_save_system_settings");
+
+
+if (defined("MCREVAPM_TESTS_ENABLED")) {
+	include_once MCREVAPM_PLUGIN_DIR."tests.php";
+}
